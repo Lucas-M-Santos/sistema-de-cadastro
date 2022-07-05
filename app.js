d3c4a8fc53ddb9021f2ea09620bd6e0e0d498128
Lucas-M-Santos <106405928+Lucas-M-Santos@users.noreply.github.com>
2022-07-05 14:06:21 -0300

    o paulo fez esse commit sem voce ver na aula

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log teste
+console.log("o paulo fez esse commit sem voce ver na aula!")
